7ee6ea92c54d405c7ce75cc2e97906112403180d
Breno Garcia <BrenoGHeat@gmail.com>
2024-01-02 11:25:38 -0300

    criado elemento com DOM

---
+++ index.js
@@ -0,0 +1,18 @@
+const article = document.createElement("article");
+const articleTitle = document.createElement("h2");
+const articleResume = document.createElement("p");
+const articleButton = document.createElement("button");
+
+articleTitle.innerText = "Como criar elementos HTML de forma dinâmica, utilizando JavaScript";
+
+articleResume.innerText = "Nesse post vamos aprender como criar elementos utilizando o DOM";
+
+articleButton.innerText = "Acessar Conteúdo";
+
+article.appendChild(articleTitle);
+article.appendChild(articleResume);
+article.appendChild(articleButton);
+
+const body = document.querySelector("body");
+
+body.appendChild(article);
